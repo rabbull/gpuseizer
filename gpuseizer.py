1a99ca55a73eab8cd8504a24bcbe4b9d2d0116b4
Karl Liu <karl_liu@outlook.com>
2019-11-06 07:47:34 +0800

    fixed several bugs

--- gpuseizer.py
+++ gpuseizer.py
@@ -2,11 +2,13 @@ import os
 import sys
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 3
-    need = sys.argv[1]
-    script = sys.argv[2]
+    assert len(sys.argv) >= 3
+    need = int(sys.argv[1])
+    script = ' '.join(sys.argv[2:])
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
     while True:
-        available = [i for i, value in enumerate([len(s.split('| ')) == 3 for s in os.popen('gpustat').readlines()[1:]]) if value]
+        available = [i for i, value in enumerate([len(s.split('| ')) == 3 for s in os.popen('gpustat').readlines()[1:]])
+                     if value]
         if need <= len(available):
             os.environ['CUDA_VISIBLE_DEVICE'] = str(available)[1:-1]
             output = os.popen(script).readlines()
